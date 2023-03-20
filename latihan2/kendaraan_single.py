9d69a130e8d4ee2413141124cf218c442cf45743
Aghisna Baihaqi_210511034 <100744486+Aghisnab@users.noreply.github.com>
2023-03-20 11:51:45 +0700

    Add files via upload

---
+++ latihan2/kendaraan_single.py
@@ -0,0 +1,20 @@
+print("Aghisna Baihaqi\n210511034\nTI21A\n")
+
+class Kendaraan:
+    def __init__(self, jenis, merk, warna):
+        self.jenis = jenis
+        self.merk = merk
+        self.warna = warna
+    def berkendara(self):
+        print("Kendaraan ini sedang berjalan.")
+
+class SepedaMotor(Kendaraan):
+    def __init__(self, jenis, merk, warna, cc):
+        super().__init__(jenis, merk, warna)
+        self.cc = cc
+    def belok(self):
+        print("Sepeda motor ini sedang belok.")
+
+motorA = SepedaMotor("Sepeda Motor", "Beat", "Biru", 200)
+motorA.berkendara() # Output: Kendaraan ini sedang berjalan.
+motorA.belok() # Output: Sepeda motor ini sedang belok.
