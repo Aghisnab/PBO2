94cf13c0ce55e72b8122cf45c44e7bf18f54bc0c
Aghisna Baihaqi_210511034 <100744486+Aghisnab@users.noreply.github.com>
2023-03-25 07:50:17 +0700

    Add files via upload

---
+++ Tugas1/kelvin_oop.py
@@ -0,0 +1,32 @@
+print("\nNama  : Aghisna Baihaqi")
+print("NIM   : 210511034")
+print("Kelas : TI21A(R1)\n\n")
+
+
+class konversi():
+    def __init__(self,K):
+        self.kelvin = K
+
+    def celcius(self):
+        C = self.kelvin-273
+        print(f'\n\nKelvin: {self.kelvin} K')
+        print(f'Konversi Kelvin ke Celcius\n\t============\t\nCelcius: {(C)} C')
+
+    def fahrenheit(self):
+        F = ((self.kelvin-273)*(9/5))+32
+        print(f'\nKelvin: {self.kelvin} K')
+        print(f'Konversi Kelvin ke Fahrenheit\n\t============\t\nFahrenheit: {F} F')
+
+    def reamur(self):
+        R = (4/5)*(self.kelvin-273)
+        print(f'\nKelvin: {self.kelvin} K')
+        print(f'Konversi Kelvin ke Reamur\n\t============\t\nReamur: {R} Re\n')
+
+
+kelvin1 = konversi(319)
+kelvin1.celcius()
+kelvin2 = konversi(292)
+kelvin2.celcius()
+kelvin2.fahrenheit()
+kelvin2.reamur()
+
